b20755f2913a877eb4a4104bb5dd17c6d2c56b9a
layah <489470342@qq.com>
2017-09-30 01:46:13 +0800

    更新后台服务

--- ddshop/app.js
+++ ddshop/app.js
@@ -4,12 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
-
+var router=express.Router()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -34,13 +31,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in developZment
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('index');
 });
 
 module.exports = app;
--- ddshop/routes/index.js
+++ ddshop/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var pool=require("../servers/api")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    console.log("服务启动")
+    var name="ly528212"
+    var addSql = 'SELECT * FROM user_info WHERE username = '+ pool.escape('ly528212');
+    pool.getConnection(function (err,connection) {
+      console.log(connection)
+        connection.query(addSql,function (err, result){
+            if(err){
+                console.log(err);
+                return;
+            }
+            else{
+               // res.json(result)
+                res.render('index', { title: JSON.stringify(result) });
+                connection.release();
+            }
+        });
 
+    })
+});
 module.exports = router;
