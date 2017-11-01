4af8c43f6281bea05100ba61e0755f5f8bc98e06
layah <layah66@126.com>
2017-11-01 11:38:30 +0800

    login
    Signed-off-by: layah <layah66@126.com>

--- ddshop/servers/api.js
+++ ddshop/servers/api.js
@@ -1,7 +1,7 @@
 // 引入文件模块
 var mysql  = require('mysql');
 module.exports = mysql.createPool({
-  host     : '47.95.115.235',
+  host     : 'localhost',
   user     : 'root',
   password : 'ly528212',
   port: '3306',
